3029f93026240ad6ac4f5985502b62516448b9eb
ivana <iva1100b@gmail.com>
2022-03-03 20:18:34 +0200

    Add exercises

--- 28.02.2022/Task-15/main.c
+++ 28.02.2022/Task-15/main.c
@@ -9,6 +9,24 @@ int main()
     колко е чистата средна печалба на Иван на ден в лева, тъй като той харчи изкараното в България. Приема
     се, че в годината има точно 365 дни. Курсът на долара спрямо лева ще се чете от конзолата.  */
 
+    int workingDays;
+    float USDPerDay, USD;
+
+    printf("Enter the average working days per month: ");
+    scanf("%d", &workingDays);
+    printf("Enter the average income per day in USD: ");
+    scanf("%f", &USDPerDay);
+    printf("Enter the current amount USD to BGN: ");
+    scanf("%f", & USD);
+
+    float incomePerMonth = workingDays * USDPerDay;
+    float incomePerYear = incomePerMonth * 12;
+    incomePerYear += incomePerMonth * 2.5;
+    incomePerYear -= incomePerYear * 0.25;
+    float totalBGNPerYear = incomePerYear * USD;
+    float averageBGNPerDay = totalBGNPerYear / 365;
+
+    printf("The average profit in BGN per day is: %g", averageBGNPerDay);
 
     return 0;
 }
